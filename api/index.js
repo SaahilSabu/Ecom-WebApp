dc4b30a324dfd738d9f716c02160658edfa83293
SaahilSabu <saahil.sabu@gmail.com>
2022-01-03 14:57:44 +0530

    New Message

---
+++ api/index.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB running succesfully"))
+  .catch((err) => {
+    console.log(err);
+  });
+app.use(cors());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/checkout", stripeRoute);
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("backendServer running on port 5000");
+});
---
+++ api/models/Product.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    cat: {
+      type: String,
+      required: true,
+    },
+    img: {
+      type: String,
+      required: true,
+    },
+    size: {
+      type: Array,
+    },
+    type: {
+      type: String,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    inStock: {
+      type: Boolean,
+      required: true,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Product", ProductSchema);
